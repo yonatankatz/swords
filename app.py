cb374735020302e9569396c0e14e2e315bc665aa
Yonatan Katz <katzyonatan@gmail.com>
2023-10-09 12:24:50 +0300

    update paths

--- app.py
+++ app.py
@@ -2,14 +2,10 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-# Serve static files (CSS, JavaScript, images, etc.)
-@app.route('/static/<path:filename>')
+# Serve docs files (CSS, JavaScript, images, etc.)
+@app.route('/<path:filename>')
 def serve_static(filename):
-    return send_from_directory('static', filename)
-
-@app.route('/')
-def hello():
-    return "Hello, World!"
+    return send_from_directory('docs', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
